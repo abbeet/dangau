e28ba8cee87858c8459431569c484453589ae211
abbeet <abbeet@gmail.com>
2024-02-05 14:58:54 +0700

    update today

---
+++ 4_webapp/pages/5_HPE_6dRepNet.py
@@ -0,0 +1,17 @@
+# Import SixDRepNet
+from sixdrepnet import SixDRepNet
+import cv2
+
+
+# Create model
+# Weights are automatically downloaded
+model = SixDRepNet()
+
+img = cv2.imread('/path/to/image.jpg')
+
+pitch, yaw, roll = model.predict(img)
+
+model.draw_axis(img, yaw, pitch, roll)
+
+cv2.imshow("test_window", img)
+cv2.waitKey(0)
